ce83fab5893b9655ca3191b6a08001872a54f7cd
SzDabrowski <szdabrowsky@gmail.com>
2023-08-02 09:13:25 +0200

    feat/added-scrit

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('Hello from Nerdbord!')
+
+const box = document.querySelector(".box");
+const colors = ['red','blue','green','yellow'];
+
+colors.forEach((color, i) => {
+    setInterval(function (){
+        box.style.borderColor = color;
+    },(i+1)*3000);
+})
+
